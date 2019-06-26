0723a4a3a54f0dbb589b0440237b767b2aaa92dc
zhangyongkang <1479834277@qq.com>
2019-06-26 17:08:54 +0800

    修改dao层的注解为@Repository

--- src/main/java/com/springboot/demo/dao/ArtworkDao.java
+++ src/main/java/com/springboot/demo/dao/ArtworkDao.java
@@ -4,10 +4,10 @@ import com.springboot.demo.model.ArtworkDO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 @Mapper
-@Component
+@Repository
 public interface ArtworkDao {
 
     @Select("select * from artwork where id = #{id}")
